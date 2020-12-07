27d3c0f0d078c6752482fc7147d8476589096745
upendra gusain <upendra.gusain@ocubis.co.uk>
2020-12-07 12:08:29 +0000

    add timer

--- src/App.tsx
+++ src/App.tsx
@@ -7,25 +7,52 @@ import Confirm from './Confirm'
 interface IState {
    confirmOpen: boolean
    confirmMessage: string
+   countDown: number
 }
 
 class App extends React.Component<{}, IState> {
+   private timer: number = 0
    constructor(props: {}) {
       super(props)
       this.state = {
          confirmOpen: true,
          confirmMessage: 'Please hit the confirm button',
+         countDown: 10,
       }
+   }
+
+   public componentDidMount() {
+      this.timer = window.setInterval(() => this.handleTimerTick(), 1000)
+   }
+
+   private handleTimerTick = () => {
+      this.setState(
+         {
+            confirmMessage: `Please hit the OK button ${this.state.countDown} secs to go`,
+            countDown: this.state.countDown - 1,
+         },
+         () => {
+            if (this.state.countDown <= 0) {
+               clearInterval(this.timer)
+               this.setState({
+                  confirmMessage: 'Too late to confirm!',
+                  confirmOpen: false,
+               })
+            }
+         },
+      )
    }
 
    private handleOkClick = () => {
       console.log('Ok Clicked!')
       this.setState({ confirmOpen: false, confirmMessage: 'Ok clicked!' })
+      clearInterval(this.timer)
    }
 
    private handleCancelClick = () => {
       console.log('Cancel clicked')
       this.setState({ confirmOpen: false, confirmMessage: 'Cancel clicked!' })
+      clearInterval(this.timer)
    }
 
    public render() {
@@ -36,7 +63,7 @@ class App extends React.Component<{}, IState> {
             <Confirm
                open={this.state.confirmOpen}
                title="React and Typescript"
-               content="Are you sure?"
+               content={this.state.confirmMessage}
                onCancelClick={this.handleCancelClick}
                onOkClick={this.handleOkClick}
             />
